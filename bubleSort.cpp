955041476713dd24cfe36ab0f4da93afdfcc94b7
hermayani2786 <herma.yani2786@gmail.com>
2026-03-05 11:49:38 +0700

    menambahkan return

--- bubleSort.cpp
+++ bubleSort.cpp
@@ -48,3 +48,32 @@ void bubleSortArray() { // procedur untuk mengurutkan array
     } while ( pass <= n - 1); // step 5
 
 }
+void display(){
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "element array yang telah tersusun" << endl;
+    cout << "===================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++){
+        cout << arr[j]; // menampilkan array
+        if (j < n - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+}
+
+int main() {
+
+    input();
+
+    bubleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
